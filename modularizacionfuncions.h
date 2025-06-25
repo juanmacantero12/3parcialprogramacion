20889141312901fc859044ce6241e8a580e9dbf9
juanmacantero12 <114021198+juanmacantero12@users.noreply.github.com>
2025-06-25 13:22:43 -0300

    Create modularizacionfuncions.h

---
+++ modularizacionfuncions.h
@@ -0,0 +1,11 @@
+/*Declaracion de funciones*/
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+
+#endif // FUNCIONES_H
+
+/*En el programa principal se declara #include "FUNCIONES.H" para poder usarlo*/
